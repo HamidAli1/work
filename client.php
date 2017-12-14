87c05a2537c5703196f76526b169dbbe9a49e9dc
HamidAli1 <34533451+HamidAli1@users.noreply.github.com>
2017-12-14 18:49:33 +0500

    Update client.php

--- client.php
+++ client.php
@@ -3,18 +3,12 @@
 $host    = "127.0.0.1";
 $port    = 25003;
 
-
-
-
-//$message =readline("         enter your message \n");
 $socket = socket_create(AF_INET, SOCK_STREAM, 0) ;
 $res = socket_connect($socket, $host, $port) ;
-
 $res = socket_read ($socket, 1024);
 echo "                     Reply From Server  : \n".$res."\n";
 $message =readline("");
 socket_write($socket, $message, strlen($message));
-
 
 while($res = socket_read ($socket, 1024))
 {
@@ -22,14 +16,10 @@ while($res = socket_read ($socket, 1024))
 	echo "                     Reply From Server  : \n".$res."\n";
 	$message =readline("");
 	socket_write($socket, $message, strlen($message));
-
 	
 }
 
 // close socket
 socket_close($socket);
 echo "Ok close";
-
-
-
 ?>
