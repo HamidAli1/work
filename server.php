ce9a62f31077b14b2d476830e1df34ea96b8177c
HamidAli1 <34533451+HamidAli1@users.noreply.github.com>
2017-12-14 18:42:18 +0500

    Update server.php

--- server.php
+++ server.php
@@ -10,17 +10,75 @@ $socket = socket_create(AF_INET, SOCK_STREAM, 0) ;
 $result = socket_bind($socket, $host, $port);
 
 echo "         Welcome in Server\n";
-while(1)
-{
+
+
 $result = socket_listen($socket, 3);
 $ans = socket_accept($socket);
+
+//echo "Please select one option \n"."1-To send the Messages\n"."2-To make Duplication \n";
+$output ="Please select one option \n"."1-To send the Messages\n"."2-To make Duplication \n";
+socket_write($ans, $output, strlen ($output));
 $input = socket_read($ans, 1024);
 $input = trim($input);
-echo "Client Message : ".$input."\n";
-$output =readline("enter message from server  :  ");
+if($input==1)
+{
+$output ="Please type your message \n";
 socket_write($ans, $output, strlen ($output));
 
+$input = socket_read($ans, 1024);
+$input = trim($input);
+echo "your message was  : ".$input."  \n";
 }
+elseif($input==2)
+{
+
+
+
+    $output ="Please give the source path for duplication \n";
+   socket_write($ans, $output, strlen ($output));
+$putter=1;
+while($k = socket_read($ans, 1024)) {
+  echo $k."\n";
+if($putter==1)
+{
+	$path1=$k;
+        $putter=2;
+	
+}
+elseif($putter==2)
+{
+	$path2=$k;
+	 mycopy($path1,$path2);
+	socket_close($ans);
+	break;
+
+}
+  $output ="Please give the destination path for duplication \n";
+  socket_write($ans, $output, strlen ($output));
+ 
+ 
+}
+   
+
+
+}
+
+
 socket_close($ans);
 socket_close($socket);
+
+function mycopy($s1,$s2) {
+
+echo "source  ".$s1."\n";
+echo "destination  ".$s2."\n";
+   $path = pathinfo($s2);
+    if (!file_exists($path['dirname'])) {
+        mkdir($path['dirname'], 0777, true);
+    }
+    if (!copy($s1,$s2)) {
+        echo "copy failed \n";
+    }
+
+}
+
 ?>
